978ad8dc8836c8d5c4dbd00b7ef587154cc7cabe
mycyberly <111699411+mycyberly@users.noreply.github.com>
2022-09-04 14:58:26 +0400

    Create crt.sh

---
+++ crt.sh
@@ -0,0 +1,10 @@
+crt.sh which is a web interface to a distributed database called the certificate transparency logs. It helps domain owners to monitor certificate issuance for their domain, and use that to detect mis-issued certificates.
+
+Things that you can monitor using this are:
+> certificate expiry of your certicates at one place.
+> mis-issued domain identification and then taking proper actoin on it.
+
+In simple words crt.sh will help you to find all the SSL or TLS certificates of the particular domain
+
+What is Certificate Transparency ?
+Certificate Transparency is an open framework for monitoring SSL Certificates. It is created for Domain Owners to monitor certificate issuance for their domain, and use that to detect misissued certificates.
